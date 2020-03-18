8f2c4a747adc5706edc7f5cfc1fdef4fd44e0f4a
Morpilou <Morpilou@users.noreply.github.com>
2020-03-18 14:01:21 +0100

    add clear canevas button

--- js/sketch.js
+++ js/sketch.js
@@ -4,22 +4,30 @@ let size = 10;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(0);
   noStroke();
+  background(0);
   hue = 0;
 }
 
 function draw() {
 
 
+
   let key = keyCode;
   switch (key) {
-    case 71: // key G (for 'gomme')
+    case 69: // key E (for 'eraser')
       strokeWeight(size);
       stroke(0); // Red color
       if (mouseIsPressed === true) {
         point(mouseX, mouseY);
       }
+      break;
+    case 67: // key C (for 'clear')
+
+      clear();
+      background(0);
+      keyCode = 0;
+
       break;
     default:
       // Rainbow dots
